1eba765d4f2250205486de1fd1ac4f5805443579
wangtianhang <690879430@qq.com>
2016-10-27 23:37:13 +0800

    添加部分测试代码

---
+++ InputList/Assets/Singleton.cs
@@ -0,0 +1,22 @@
+﻿/*
+   Tuner Data -  Read Static Data in Game Development.
+   e-mail : dongliang17@126.com
+*/
+using System.Diagnostics;
+using UnityEngine;
+namespace Tuner
+{
+    public class Singleton<T> where T : new()
+    {
+        protected Singleton()
+        {
+            //Debug.Assert(null == instance);
+        }
+        private static T internalInstance = new T();
+        public static T Instance
+        {
+            get { return internalInstance; }
+        }
+    }
+
+}
