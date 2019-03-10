3f04852bc5f30339d74015868f6906f400dcc8ac
wth <690879430@qq.com>
2019-03-10 16:44:41 +0800

    添加ugui测试工程

---
+++ uguiLearn/uguiLearn/Assets/UGUI/TestBtn.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBtn : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClickBtn()
+    {
+        Debug.Log("TestBtn.OnClick");
+    }
+}
