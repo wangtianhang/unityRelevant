27ab41d3c3cba749ea0025379bfe56fc51fd6ee8
wth <690879430@qq.com>
2019-06-10 22:27:16 +0800

    添加统计内存方案

---
+++ profilerStatistic/profilerStatistic/Assets/testStatistic/HookUtils.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HookUtils
+{
+    public static void Begin(string tag)
+    {
+        Debug.Log("HookUtil begin " + tag);
+    }
+
+    public static void End(string tag)
+    {
+        Debug.Log("HookUtil end " + tag);
+    }
+
+    public static void ToMessage()
+    {
+
+    }
+}
---
+++ profilerStatistic/profilerStatistic/Assets/testStatistic/TestAllocMemory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Test
+{
+    class TestAllocMemory : MonoBehaviour
+    {
+        private void Update()
+        {
+            List<string> test = new List<string>();
+            test.Add("gaga");
+        }
+    }
+}
