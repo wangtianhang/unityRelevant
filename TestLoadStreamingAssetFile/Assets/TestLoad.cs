5b5b06752318eadee928f244640e5e602840b159
wth <690879430@qq.com>
2019-10-24 12:16:23 +0800

    添加同步读取streamingasset下工具

---
+++ TestLoadStreamingAssetFile/Assets/TestLoad.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class TestLoad : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+	{
+		string path = "test/test.txt";
+		AndroidJavaClass javaClass = new AndroidJavaClass("com.example.loadstreamingassetfile.LoadStreamingAssetFile");
+		byte[] data = javaClass.CallStatic<byte[]>("loadFile", path);
+		string str = System.Text.Encoding.UTF8.GetString ( data );
+		Debug.Log("TestLoad Start " + str);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		
+	}
+}
